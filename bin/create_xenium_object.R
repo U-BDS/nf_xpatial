b9d053ff05895d7e3e1714ce6a047250970becdb
Austyn Trull <atrull@uab.edu>
2025-07-24 14:33:09 -0500

    (Issue #12) SlideID is now required Metadata

--- bin/create_xenium_object.R
+++ bin/create_xenium_object.R
@@ -119,7 +119,7 @@ sample_metadata <- read.csv(opt$metadata)
 # TODO: Figure out what columns are needed
 # Validate metadata
 # Define required columns
-req_cols <- c("SampleID")
+req_cols <- c("SampleID", "SlideID")
 
 # Check for missing columns
 missing_cols <- setdiff(req_cols, colnames(sample_metadata))
