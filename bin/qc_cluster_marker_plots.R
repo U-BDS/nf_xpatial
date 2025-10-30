04fa66616e1ddc71a8e7df15f7853424243902c2
Austyn Trull <atrull@uab.edu>
2025-10-30 11:27:58 -0500

    Adding modules for new harmony and banksy plots

---
+++ bin/qc_cluster_marker_plots.R
@@ -0,0 +1,142 @@
+#!/usr/bin/env Rscript
+
+set.seed(1234)
+
+######################
+### LOAD LIBRARIES ###
+######################
+
+# General Utilities
+library(optparse)   # Commandline arguments
+library(Seurat)     # Main analysis package
+library(stringr)
+
+# Plotting
+library(ggplot2)
+library(patchwork)
+library(RColorBrewer)
+library(Polychrome)
+library(pals)
+
+###############################
+### COMMAND-LINE PARAMETERS ###
+###############################
+
+params_list <- list(
+    make_option(
+        c("-i", "--input"),
+        type="character",
+        default=NULL,
+        metavar="path",
+        help="R Object to be analyzed"),
+    make_option(
+        c("-g", "--gene_list"),
+        type="character",
+        default=NULL,
+        metavar="path",
+        help="The marker gene list"),
+    make_option(
+        c("-o", "--outfile"),
+        type="character",
+        default="harmony_plots.png",
+        metavar="path",
+        help="The output name for the harmony_plot"),
+    make_option(
+        c("--width"),
+        type="integer",
+        default=2500,
+        help="Width of the plot"),
+    make_option(
+        c("--height"),
+        type="integer",
+        default=3000,
+        help="Height of the plot"),
+    make_option(
+        c("-a", "--assay"),
+        type="character",
+        default="Xenium",
+        help="The assay name to be merged")
+    )
+
+opt_parser <- OptionParser(option_list=params_list)
+opt <- parse_args(opt_parser)
+
+if (is.null(opt$input)) {
+    print_help(opt_parser)
+    stop("Please provide a xenium object as input.", call. = FALSE)
+}
+
+##################
+### LOAD INPUT ###
+##################
+
+# Read in the xenium object
+xenium_obj <- readRDS(
+    file = opt$input
+)
+
+# Read in marker gene list
+marker_gene_list <- read.csv(opt$gene_list, sep = ",", header=FALSE)
+marker_gene_list <- unlist(marker_gene_list, use.names = FALSE)
+
+#################
+#### VLN PLOT ###
+#################
+
+# Get colors
+cols <- NULL
+if (length(unique(xenium_obj$seurat_clusters)) <= 32) {
+    cols <- as.vector(pals::glasbey(length(unique(xenium_obj$seurat_clusters))))
+
+} else if (length(unique(xenium_obj$seurat_clusters)) <= 36) {
+    cols <- as.vector(pals::polychrome(length(unique(xenium_obj$seurat_clusters))))
+
+} else {
+    cols <- as.vector(
+        Polychrome::createPalette(
+            N = length(unique(xenium_obj$seurat_clusters)),
+            seedcolors = c("#FF0000", "#00FF00", "#0000FF")
+        )
+    )
+}
+
+# Check if the input was a list of objects or a single object
+vln_plot <- VlnPlot(
+    xenium_obj,
+    layer = "data",
+    features = rev(as.character(unique(marker_gene_list))),
+    group.by = "seurat_clusters",
+    assay = opt$assay,
+    stack = TRUE,
+    combine = TRUE,
+    fill.by = "ident",
+    cols = cols,
+    flip = TRUE,
+    y.max = 1.5,
+    same.y.lims = F,
+    pt.size = 0.00005
+) + NoLegend()
+
+###################
+### OUTPUT PLOT ###
+###################
+
+# Output the plot
+ggsave(
+    opt$outfile,
+    plot = vln_plot,
+    width = opt$width,
+    height = opt$height,
+    units = "px",
+    limitsize = FALSE
+)
+
+####################
+### SESSION INFO ###
+####################
+
+sessioninfo <- "R_sessionInfo.log"
+
+sink(sessioninfo)
+sessionInfo()
+sink()
---
+++ modules/local/qc_split_cluster_plots.nf
@@ -0,0 +1,40 @@
+process QC_SPLIT_CLUSTER_PLOTS {
+    tag "$meta.id"
+    label 'process_high'
+
+    container "${ 
+        (workflow.containerEngine == 'singularity') &&
+            (!task.ext.singularity_pull_docker_container) ?
+            'docker://uabbds/nf_xenium_analysis:0.0.2' :
+            'docker.io/uabbds/nf_xenium_analysis:0.0.2' 
+        }"
+
+    input:
+    tuple val(meta), path(xenium_obj)
+
+    output:
+    tuple val(meta), path("*.png"), emit: split_cluster_plot
+    path 'versions.yml'           , emit: versions
+
+    when:
+    task.ext.when == null || task.ext.when
+
+    script:
+    def args   = task.ext.args ?: ""
+    def prefix = task.ext.prefix ?: "${meta.id}"
+    def assay_flag = meta.normalization == 'area_norm' ? '--assay AreaNorm' : '--assay Xenium'
+
+    """
+    qc_split_cluster_plot.R \\
+        $args \\
+        --input "$xenium_obj" \\
+        $assay_flag \\
+        --outfile ${prefix}_split_cluster_plot.png
+
+    cat <<-END_VERSIONS > versions.yml
+    "${task.process}":
+        r-base: \$(echo \$(R --version 2>&1) | sed 's/^.*R version //; s/ .*\$//')
+        r-seurat: \$(Rscript -e "library(Seurat); cat(as.character(packageVersion('Seurat')))")
+    END_VERSIONS
+    """
+}
